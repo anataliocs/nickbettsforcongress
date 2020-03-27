07b85dc28288d18f64ce55427b7ea5398db63907
chrisanatalio <anataliocs@gmail.com>
2020-03-26 20:35:12 -0700

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,12 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-162037864-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
